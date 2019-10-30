f77bfc34282018c8dd5ee9032a9928b1f360a300
nguyen <hoangkhoinguyen3001@gmail.com>
2019-10-30 19:39:10 +0700

    practice some output

--- day1.php
+++ day1.php
@@ -1,2 +1,36 @@
+
+<!DOCTYPE html>
+<html>
+<body>
+
 <?php
+/*
 echo 123;
+*/
+/*$color = "red";
+echo "my car is". $color ."<br>";
+echo "my house is". $color ."<br>";
+echo "my boat is". $color ."<br>";
+*/
+/*
+$x = 5 * 5 *5;
+echo $x;
+*/
+/*
+$txt = "W3Schools.com";
+echo "i love" . $txt . "!";
+echo "<br>";
+echo "i love $txt";
+*/
+/*$x = 4;
+$y = 5;
+echo $x + $y;
+*/
+
+?>
+
+
+</body>
+</html>
+
+
