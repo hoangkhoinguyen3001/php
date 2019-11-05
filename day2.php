274fcd492612d02a005cc34d45ad3d4104a20a22
nguyen <hoangkhoinguyen3001@gmail.com>
2019-11-05 19:33:06 +0700

    day2

---
+++ day2.php
@@ -0,0 +1,19 @@
+<?php
+$i = 4;
+$d = 4.0;
+$s = "HackerRank ";
+// Read and save an integer, double, and String to your variables.
+$handle = fopen("php://stdin", "r");
+$i2 = fgets($handle);
+$d2 = fgets($handle);
+$s2 = fgets($handle);
+// Print the sum of both integer variables on a new line.
+printf("%d\n", $i + $i2);
+// Print the sum of the double variables on a new line.
+printf("%.1f\n", $d + $d2);
+// Concatenate and print the String variables on a new line
+// The 's' variable above should be printed first.
+print($s.$s2."\n");
+fclose($handle);
+
+
